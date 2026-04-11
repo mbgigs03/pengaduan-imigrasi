b8bcb30f082a5d160ef190f5d8b1ef31907bf8f6
mbgigs03 <mbgigihbaskoroashari@gmail.com>
2026-04-11 15:35:12 +0700

    initial commit - setup laravel pengaduan

---
+++ database/migrations/2026_04_11_081907_create_tindak_lanjuts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tindak_lanjut', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pengaduan_id')->constrained()->cascadeOnDelete();
+            $table->text('catatan_petugas');
+            $table->timestamp('tanggal_selesai')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tindak_lanjuts');
+    }
+};
